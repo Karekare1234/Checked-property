67ac7a36adf1f60a3cfb03db8eae4982be7d9a9a
Karell <161704406+Karekare1234@users.noreply.github.com>
2024-06-08 00:13:43 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// .checked = property that determins the checked statement of an
+//            HTML checkbox or radio button element.
+
+
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function () {
+    if (myCheckbox.checked) {
+        subResult.textContent = `You are Subscribed`;
+    }
+    else{
+        subResult.textContent =`You are NOT Subscribe!`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with MasterCard`;
+    }
+    else if( payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+    else{
+        paymentResult.textContent = `You must select the payment type`;
+    }
+
+}
